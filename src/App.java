64631cfd8755a13186123284e7c18c8b47ac06a0
Baldanz00 <gioia.travaglini18@gmail.com>
2025-09-07 16:25:55 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,92 @@
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+/*Creare una classe Main di test, in cui si chiede all’utente di inserire un nuovo evento con tutti i parametri. */
+System.out.println("Inserisci i dati dell'evento: titolo, data, numero di posti totali. \n Inserisci il titolo: ");
+
+String titolo = scan.nextLine();
+
+System.out.println("Inserisci la data dell'evento divisa per giorno , mese , anno. \n Inserisci il giorno:");
+int giorno = scan.nextInt();
+System.out.println("Inserisci il mese:");
+int mese = scan.nextInt();
+System.out.println("Inserisci l'anno:");
+int anno = scan.nextInt();
+LocalDate dataEvento = LocalDate.of(anno, mese, giorno);
+
+System.out.println("Inserisci il numero di posti totali.");
+int totalePosti = scan.nextInt();
+scan.nextLine();
+
+Evento evento1 = new Evento(titolo, dataEvento, totalePosti);
+System.out.println("\n Evento creato.");
+System.out.println("Dettagli evento: " + evento1);
+
+/*Dopo che l’evento è stato istanziato, chiedere all’utente se e quante prenotazioni vuole fare e provare ad effettuarle,
+implementando opportuni controlli */
+
+System.out.println("Vuoi effettuare una prenotazione? Scrivi : \n Si -  effettuare una prenotazione; \n No - non effettuare nessuna prenotazione.");
+String rispostaUtente = scan.nextLine();
+if(rispostaUtente.equalsIgnoreCase("Si")){
+    boolean prenotazioni = false;
+    while(!prenotazioni){
+        System.out.println("Quanti posti vuoi prenotare?");
+        int prenotazioniEffettuate = scan.nextInt();
+        scan.nextLine();
+
+        if(prenotazioniEffettuate == 0){
+            System.out.println("Nessuna prenotazione effettuata.");
+            prenotazioni = true;
+        } else if( prenotazioniEffettuate < 0){
+            throw new IllegalArgumentException("Il numero di prenotazioni non può essere negativo");
+        }else if( prenotazioniEffettuate > (evento1.getTotalePosti()-evento1.getPostiPrenotati())){
+            throw new IllegalArgumentException("Non ci sono posti disponibili");
+        } else {
+            for (int i = 0; i < prenotazioniEffettuate; i++){
+                evento1.prenota();
+            }
+            System.out.println("Prenotazioni effettuate.");
+            prenotazioni = true;
+        }
+    }
+
+    /*Stampare a video il numero di posti prenotati e quelli disponibili */
+    System.out.println("Posti prenotati: " + evento1.getPostiPrenotati());
+    System.out.println("Posti disponibili: " + evento1.getTotalePosti());
+}
+/*Chiedere all’utente se e quanti posti vuole disdire */
+System.out.println("Vuoi disdire una prenotazione? Scrivi \n Si - disdire; \n No - non disdire");
+String disdirePrenotazioneUtente = scan.nextLine();
+if(disdirePrenotazioneUtente.equalsIgnoreCase("Si")){
+    boolean prenotazioni = false;
+    while(!prenotazioni){
+        System.out.println("Quante prenotazioni vuoi disdire?");
+        int disdette = scan.nextInt();
+    
+        if(disdette == 0){
+            System.out.println("Non hai effettuato nessuna disdetta.");
+            prenotazioni = true;
+        } else if( disdette < 0 ){
+            throw new IllegalArgumentException("Numero disdette non valido.");
+        }else if(disdette > evento1.getPostiPrenotati()){
+            throw new IllegalArgumentException("Non ci sono prenotazioni da disdire");
+            
+        }else {
+            for (int i = 0; i < disdette; i++){
+                evento1.disdici();
+            }
+            System.out.println("Disdetta della prenotazione effettuata");
+            prenotazioni = true;
+            
+        }
+    }
+}
+
+    System.out.println("Posti prenotati: " + evento1.getPostiPrenotati());
+    System.out.println("Posti disponibili: " + evento1.getTotalePosti());
+    }
+}
---
+++ src/Evento.java
@@ -0,0 +1,111 @@
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Evento {
+
+    private String titolo;
+    private LocalDate date;
+    private int totalePosti;
+    private int postiPrenotati;
+
+    /*Quando si istanzia un nuovo evento questi attributi devono essere tutti valorizzati nel costruttore, tranne posti prenotati
+    che va inizializzato a 0. */
+    public Evento(String titolo, LocalDate date, int totalePosti) {
+        this.titolo = titolo;
+        this.date = date;
+        this.totalePosti = totalePosti;
+        this.postiPrenotati = 0;
+    
+/*Inserire il controllo che la data non sia già passata e che il numero di posti totali sia positivo. 
+In caso contrario sollevare eccezione. */
+    if(date == null || date.isBefore(LocalDate.now())){
+    throw new IllegalArgumentException("La data non è valida");
+} else {
+    this.date = date;
+}
+
+    if(totalePosti <= 0){
+        throw new IllegalArgumentException("Il numero di posti totali non può essere negativo o 0.");
+    } else {
+        this.totalePosti = totalePosti;
+    }
+}
+
+    /*Aggiungere metodi getter e setter in modo che:
+    titolo sia in lettura e in scrittura
+    data sia in lettura e scrittura
+    numero di posti totale sia solo in lettura
+    numero di posti prenotati sia solo in lettura
+ */
+
+    
+    
+    public void setTitolo(String titolo){
+        if(titolo == null || titolo.isBlank()){ //System.out.println("Entra nell'if");
+            System.out.println("Il titolo non può essere vuoto.");
+        } else if(titolo.matches("\\d+")){
+            System.out.println("Il titolo non può contenere solo numeri");
+        }else { //System.out.println("Esce dall'if");
+            this.titolo = titolo;
+        }
+    }
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+    public LocalDate setDate(){
+        return date;
+    }
+
+    public int getTotalePosti() {
+        return totalePosti;
+    }
+
+    public int getPostiPrenotati() {
+        return postiPrenotati;
+    }    
+
+    /*Vanno inoltre implementati dei metodi public che svolgono le seguenti funzioni:
+ - prenota: aggiunge uno ai posti prenotati. Se l’evento è già passato o non ha posti 
+            disponibili deve sollevare eccezione. */
+
+public void prenota(){
+if(date.isBefore(LocalDate.now())){
+    System.out.println("Non puoi prenotare un posto per un evento già passato.");
+} else if (postiPrenotati >= totalePosti){
+    System.out.println("Non ci sono più posti disponibili per la prenotazione.");
+} else {
+    postiPrenotati++;
+    totalePosti--;
+}
+System.out.println("Prenotazione per l'evento effettuata.");
+}
+
+/* - disdici: riduce di uno i posti prenotati. Se l’evento è già passato o non ci sono prenotazioni 
+solleva eccezione.
+ */
+
+ public void disdici() {
+    if(date.isBefore(LocalDate.now())){
+        System.out.println("Non puoi disdire la prenotazione perchè l'evento è già passato.");
+    } else if(postiPrenotati == 0){
+        System.out.println("Non ci sono prenotazioni effettute.");
+    }else {
+        postiPrenotati --;
+        totalePosti ++;
+        System.out.println("Disdetta effettuata.");
+    }
+ }
+
+ /* - l’override del metodo toString() in modo che venga restituita una stringa contenente: data formattata - titolo */
+
+ @Override
+ public String toString(){
+    DateTimeFormatter dataFormattata = DateTimeFormatter.ofPattern("dd/MM/yyy");
+    return "Data: " + date.format(dataFormattata) + " - Titolo : " + titolo;
+ }
+}
